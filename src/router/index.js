8cb5716b7f51f56c4df20a9d51734f31c7a38f7b
sadirov4647 <sadirovizzatillo3@gmail.com>
2021-09-28 14:02:11 +0300

    adding

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '@/views/EventList.vue'
-import EventDisplay from '@/views/EventDisplay.vue'
+import EventDisplay from '@/views/event/Display.vue'
+import EventRegister from '@/views/event/Register.vue'
+import EventEdit from '@/views/event/Edit.vue'
 import About from '@/views/About.vue'
 
 const routes = [
   {
     path: '/',
     name: 'EventList',
-    component: EventList
+    component: EventList,
+    props: route => ({ page: parseInt(route.query.page) || 1 })
   },
   {
     path: '/event/:id',
     name: 'EventDisplay',
-    props:true,
+    props: true,
     component: EventDisplay
+  },
+  {
+    path: '/event/:id/register',
+    name: 'EventRegister',
+    props: true,
+    component: EventRegister
+  },
+  {
+    path: '/event/:id/edit',
+    name: 'EventEdit',
+    props: true,
+    component: EventEdit
   },
   {
     path: '/about',
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,35 +1,66 @@
 <template>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Previous</router-link
+      >
+
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next &#62;</router-link
+      >
+    </div>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from 'vue'
 // import axios from 'axios'
 
 export default {
   name: 'EventList',
+  props:['page'],
   components: {
     EventCard
   },
   data() {
     return {
-      events: null
+      events: null,
+      totalEvents:0
     }
   },
   created() {
-    EventService.getEvents()
+    watchEffect(() =>{
+      this.events = null
+    EventService.getEvents(2, this.page)
       // .get(
       //   'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3/events'
       // )
       .then(response => {
         this.events = response.data
+        this.totalEvents = response.headers['x-total-count']
       })
       .catch(error => {
         console.log(error)
       })
+    })
+  },
+  computed:{
+    hasNextPage(){
+      var totalPages  = Math.ceil(this.totalEvents / 2 )
+      return this.page < totalPages
+    }
   }
 }
 </script>
@@ -39,5 +70,23 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
